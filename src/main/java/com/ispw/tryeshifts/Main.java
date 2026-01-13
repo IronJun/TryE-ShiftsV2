79e06dab3cd3d0481f02a1e65db5699b79e7efea
ArduSan <ferretti2003simone@gmail.com>
2026-01-13 17:50:08 +0100

    Issues handling

--- src/main/java/com/ispw/tryeshifts/Main.java
+++ src/main/java/com/ispw/tryeshifts/Main.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    private Main(){
+        throw new IllegalStateException("Utility class");
+    }
     public void start(Stage primaryStage) throws Exception
     {
         SceneManager manager = SceneManager.getInstance();
